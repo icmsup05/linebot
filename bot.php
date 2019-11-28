9c8552f6b0a6af488dd2d60d68d07e556fbd90b1
icmsup05 <56708689+icmsup05@users.noreply.github.com>
2019-11-28 17:32:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,8 +23,11 @@ if ( sizeof($request_array['events']) > 0 ) {
         $text = $event['message']['text'];
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+            //'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
             //'messages' => [['type' => 'text', 'text' => $text ]]
+            'messages' => [
+            ['type' => 'text', 
+             'text' => json_encode($request_array)]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
