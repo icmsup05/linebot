7c5b51e496919be51d198664efd2de9d64b06fed
icmsup05 <56708689+icmsup05@users.noreply.github.com>
2020-02-19 14:45:07 +0700

    Update icmbot.php

--- icmbot.php
+++ icmbot.php
@@ -43,8 +43,36 @@ if ( sizeof($request_array['events']) > 0 ) {
         $data = [
             'replyToken' => $reply_token,
             //'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => [['type' => 'sticker', 'packageId' => '11537', 'stickerId' => '52002738' ]],
-            'messages' => [['type' => 'text', 'text' => $text ]],
+            'messages' => [['type' => 'sticker', 'packageId' => '11537', 'stickerId' => '52002738' ]]
+        ];
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+        echo "Result: ".$send_result."\r\n";
+
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+        $text = $event['message']['text'];
+        $data = [
+            'replyToken' => $reply_token,
+            //'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+            'messages' => [['type' => 'text', 'text' => $text ]]
+        ];
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+        echo "Result: ".$send_result."\r\n";
+
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+        $text = $event['message']['text'];
+        $data = [
+            'replyToken' => $reply_token,
+            //'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
             'messages' => [['type' => 'text', 'text' => $user_id.','.$name_ ]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
